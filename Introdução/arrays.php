d5b116702476f8920d7986234bb0d04be8121177
Leopoldinoo <otavioleopoldino1@gmail.com>
2025-11-18 09:51:36 -0300

    Primeiras Impressões sobre PHP

---
+++ Introdução/arrays.php
@@ -0,0 +1,12 @@
+<?php
+
+
+
+$lista1 = ['otavio', 'paiva', 'acin'];
+$lista2 = ['larilari', 'ana laura', 'melissa'];
+$lista3 = [...$lista1, 'fulano', ...$lista2];
+
+print_r($lista3);
+
+
+?>
--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$idade = 10; // Número
-$nome = 'Otávio'; // String
-$dormindo = true; // Boolean
-$Dormindo = false; // Boolean
-$Vazia = null; // Variável sem valor nenhum
-echo 'Otávio Leopoldino';
-?>

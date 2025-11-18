8e9c73172a56b869bef318300e0825fde8c35db2
Leopoldinoo <otavioleopoldino1@gmail.com>
2025-11-17 21:52:37 -0300

    ...

--- arrays.php
+++ arrays.php
@@ -1,15 +1,12 @@
 <?php
 
-$ingredientes = [
-  "açucar",
-  'farinha',
-  'leite',
-  'fermento',
-  'ovo'
-];
 
 
-$numerosDaMegasena = [10, 14, 20, 45, 1];
+$lista1 = ['otavio', 'paiva', 'acin'];
+$lista2 = ['larilari', 'ana laura', 'melissa'];
+$lista3 = [...$lista1, 'fulano', ...$lista2];
 
-echo "Número 1: " . $numerosDaMegasena[0] . "<br/>";
-echo "Número 2: " . $numerosDaMegasena[1] . "<br/>";
+print_r($lista3);
+
+
+?>

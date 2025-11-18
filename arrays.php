18cc6bb0cc979de62f28372ccae245decf185dd0
Otávio Leopoldino <otavioleopoldino1@gmail.com>
2025-11-17 21:11:45 -0300

    Add files via upload

---
+++ arrays.php
@@ -0,0 +1,15 @@
+<?php
+
+$ingredientes = [
+  "açucar",
+  'farinha',
+  'leite',
+  'fermento',
+  'ovo'
+];
+
+
+$numerosDaMegasena = [10, 14, 20, 45, 1];
+
+echo "Número 1: " . $numerosDaMegasena[0] . "<br/>";
+echo "Número 2: " . $numerosDaMegasena[1] . "<br/>";
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+$idade = 10; // Número
+$nome = 'Otávio'; // String
+$dormindo = true; // Boolean
+$Dormindo = false; // Boolean
+$Vazia = null; // Variável sem valor nenhum
+echo 'Otávio Leopoldino';
+?>

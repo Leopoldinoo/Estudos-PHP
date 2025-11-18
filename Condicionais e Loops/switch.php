a23c7f24ca79e500e072a94b1c968003590b3134
Leopoldinoo <otavioleopoldino1@gmail.com>
2025-11-18 11:52:22 -0300

    Condição Switch Case

---
+++ Condicionais e Loops/switch.php
@@ -0,0 +1,16 @@
+<?php
+
+$tipo = 5;
+
+// Testando Switch Case, usa-se quando: eu tenho 1 valor, mas vários resultados
+switch ($tipo) {
+  case 10:
+    echo 'Exibindo o número 10';
+    break;
+  case 3:
+    echo 'Exibindo o número 3';
+    break;
+  case 5:
+    echo 'Exibindo o número 5';
+    break;
+}

b4678e0e65ebc2785e7907a1a26d63fb5b263529
Leopoldinoo <otavioleopoldino1@gmail.com>
2025-11-18 11:04:14 -0300

    Condicional if e else

--- Condicionais e Loops/if.php
+++ Condicionais e Loops/if.php
@@ -1,10 +1,11 @@
 <?php
 
 
-$idade = 20;
+$idade = 20; // Criando variável
 
-$maiordeIdade = ($idade >= 16) ?  true : false;
+$maiordeIdade = ($idade >= 16) ?  true : false; // Usando Boolean
 
+// Testando a Condicão
 if ($maiordeIdade) {
   echo 'Maior';
 } else {

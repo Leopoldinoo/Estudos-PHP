a84b3ccd5582f8953ab9097a5c80a6c61e32b3d0
Leopoldinoo <otavioleopoldino1@gmail.com>
2025-11-18 22:04:55 -0300

    Exercicio 2 do curso de PHP

---
+++ Condicionais e Loops/exercicio1.php
@@ -0,0 +1,44 @@
+<?php
+
+$tracinho = '-';
+
+for ($i = 1; $i <= 10; $i++) {
+  echo $tracinho;
+};
+echo '<br/>';
+
+for ($i = 1; $i <= 10; $i++) {
+  echo $tracinho;
+};
+echo '<br/>';
+for ($i = 1; $i <= 10; $i++) {
+  echo $tracinho;
+};
+echo '<br/>';
+for ($i = 1; $i <= 10; $i++) {
+  echo $tracinho;
+};
+echo '<br/>';
+for ($i = 1; $i <= 10; $i++) {
+  echo $tracinho;
+};
+echo '<br/>';
+for ($i = 1; $i <= 10; $i++) {
+  echo $tracinho;
+};
+echo '<br/>';
+for ($i = 1; $i <= 10; $i++) {
+  echo $tracinho;
+};
+echo '<br/>';
+for ($i = 1; $i <= 10; $i++) {
+  echo $tracinho;
+};
+echo '<br/>';
+for ($i = 1; $i <= 10; $i++) {
+  echo $tracinho;
+};
+echo '<br/>';
+for ($i = 1; $i <= 10; $i++) {
+  echo $tracinho;
+};
---
+++ Condicionais e Loops/exercicio2.php
@@ -0,0 +1,86 @@
+<?php
+
+
+$tracinho = '-';
+
+for ($i = 1; $i <= 1; $i++) {
+  echo $tracinho;
+};
+echo '<br/>';
+for ($i = 1; $i <= 2; $i++) {
+  echo $tracinho;
+};
+echo '<br/>';
+for ($i = 1; $i <= 3; $i++) {
+  echo $tracinho;
+};
+echo '<br/>';
+for ($i = 1; $i <= 4; $i++) {
+  echo $tracinho;
+};
+echo '<br/>';
+for ($i = 1; $i <= 5; $i++) {
+  echo $tracinho;
+};
+echo '<br/>';
+for ($i = 1; $i <= 6; $i++) {
+  echo $tracinho;
+};
+echo '<br/>';
+for ($i = 1; $i <= 7; $i++) {
+  echo $tracinho;
+};
+echo '<br/>';
+for ($i = 1; $i <= 8; $i++) {
+  echo $tracinho;
+};
+echo '<br/>';
+
+for ($i = 1; $i <= 9; $i++) {
+  echo $tracinho;
+};
+echo '<br/>';
+for ($i = 1; $i <= 10; $i++) {
+  echo $tracinho;
+};
+echo '<br/>';
+for ($i = 1; $i <= 11; $i++) {
+  echo $tracinho;
+};
+echo '<br/>';
+
+for ($i = 1; $i <= 12; $i++) {
+  echo $tracinho;
+};
+echo '<br/>';
+for ($i = 1; $i <= 13; $i++) {
+  echo $tracinho;
+};
+echo '<br/>';
+for ($i = 1; $i <= 14; $i++) {
+  echo $tracinho;
+};
+echo '<br/>';
+for ($i = 1; $i <= 15; $i++) {
+  echo $tracinho;
+};
+echo '<br/>';
+for ($i = 1; $i <= 16; $i++) {
+  echo $tracinho;
+};
+echo '<br/>';
+for ($i = 1; $i <= 17; $i++) {
+  echo $tracinho;
+};
+echo '<br/>';
+for ($i = 1; $i <= 18; $i++) {
+  echo $tracinho;
+};
+echo '<br/>';
+for ($i = 1; $i <= 19; $i++) {
+  echo $tracinho;
+};
+echo '<br/>';
+for ($i = 1; $i <= 20; $i++) {
+  echo $tracinho;
+};
